c3f948137a2bd934edcf6131d522c2cf627b53a6
Kevin Christian Sianipar <kevincsianipar@gmail.com>
2018-05-18 14:14:22 +0700

    add structure
    make structure and start search bar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//search bar
+import SearchBar from './components/searchBar/SearchBar';
+import './components/searchBar/SearchBar.css';
+
+const API_KEY = 'AIzaSyARpNovs7cj_dPnplGjEbvYU6i3WUNPb_8';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <SearchBar />
         </header>
-        <p className="App-intro">
-          
-        </p>
       </div>
     );
   }
---
+++ src/components/searchBar/SearchBar.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            searchTerm:''
+        }
+    }
+
+    render(){
+        return (
+            <div className="button_box2">
+                <form className="form-wrapper-2 cf">
+                    <input
+                        value={this.state.searchTerm} 
+                        onChange={event => this.setState({ searchTerm: event.target.value })} type="text" placeholder="Search here..." required>
+                    </input>
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+        );
+    }
+
+    onInputChange(event){
+        
+    }
+}
+
+// const SearchBar = props => {
+//   return (
+//     <div className="button_box2">
+//         <form className="form-wrapper-2 cf">
+//             <input type="text" placeholder="Search here..." required></input>
+//             <button type="submit">Search</button>
+//         </form>
+//     </div>
+//   );
+// };
+
+export default SearchBar;
